c3e5fe656c0766cbab91cd2ffab4414eebee73c8
naseokju <lukas95@naver.com>
2023-03-24 15:53:31 +0900

    complete

--- build/index.js
+++ build/index.js
@@ -1,8 +1,42 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto_1 = __importDefault(require("crypto"));
 class Block {
-    constructor(data) {
+    constructor(prevHash, height, data) {
+        this.prevHash = prevHash;
+        this.height = height;
         this.data = data;
+        this.hash = Block.calculateHash(prevHash, height, data);
     }
-    static hello() {
-        return "hi";
+    static calculateHash(prevHash, height, data) {
+        // 해쉬 값
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto_1.default.createHash("sha256").update(toHash).digest("hex");
     }
 }
+class Blockchain {
+    constructor() {
+        this.blocks = [];
+    }
+    getPrevHash() {
+        if (this.blocks.length === 0)
+            return "";
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    addBlock(data) {
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(newBlock);
+    }
+    // 블록에 접근할 수 있는 함수
+    getBlocks() {
+        return this.blocks;
+    }
+}
+const blockchain = new Blockchain();
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+console.log(blockchain.getBlocks);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,55 @@
-class Block {
-    constructor (private data:string) {}
-    static hello() {
-        return "hi";
+import crypto from "crypto";
+
+interface BlockShape {
+    hash: string;
+    prevHash: string;
+    height: number;
+    data: string;
+}
+
+class Block implements BlockShape {
+    public hash: string;
+    constructor(
+        public prevHash: string,
+        public height: number,
+        public data: string 
+    ) {
+        this.hash = Block.calculateHash(prevHash, height, data)
+    }
+    static calculateHash(prevHash:string, height:number, data:string) {
+        // 해쉬 값
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
+
+class Blockchain {
+    private blocks: Block[]
+    constructor() {
+        this.blocks = [];
+    }
+    private getPrevHash() {
+        if(this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length - 1].hash;
+        
+    }
+    public addBlock(data:string) {
+        const newBlock = new Block(this.getPrevHash(), 
+        this.blocks.length + 1, data);
+        this.blocks.push(newBlock);
+    }
+    // 블록에 접근할 수 있는 함수
+    public getBlocks() {
+        return [...this.blocks];
+    }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+
+blockchain.getBlocks().push(new Block("xxxx", 1111, "HACKEDD"));
+
+console.log(blockchain.getBlocks());
